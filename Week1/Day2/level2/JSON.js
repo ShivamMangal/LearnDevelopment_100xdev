4d3c1e42ca37933d910046df9b3a5a3594db1a89
ShivamMangal <shivammangal0708@gmail.com>
2025-09-08 14:04:59 +0530

    dono

--- Week1/Day2/level2/JSON.js
+++ Week1/Day2/level2/JSON.js
@@ -1,10 +1,18 @@
-const user ={
-    name:"Shivam",
-    age:23,
-    gender:"Male"
-}
-console.log(user["age"]);
-console.log(user.gender);
-console.log(user["name"]);
+//Create Object👇
+// const user ={
+//     name:"Shivam",
+//     age:23,
+//     gender:"Male"
+// }
+// console.log(user["age"]);
+// console.log(user.gender);
+// console.log(user["name"]);
 
+//String Object👇
+const users = '{ "name":"Shivam","age":23}';
 
+// JSON.parse -> convert String object into Normal obect so we can access like normal object
+// JSON.stringify -> convert object into string
+
+const user = JSON.parse(users);
+console.log(user.name);
